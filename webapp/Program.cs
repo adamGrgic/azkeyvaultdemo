4d2782369686acba1e657494a7566c7c33430bc8
Adam Grgić <adam@grgicconsulting.com>
2024-03-01 19:09:52 -0700

    add key package and logic

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,5 +1,25 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+};
+var client = new SecretClient(new Uri("https://kvtestdemo6969.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+var secret = client.GetSecret("ExamplePassword");
+
+var secretValue = secret.Value;
 
 app.MapGet("/", () => "Hello World!");
 
